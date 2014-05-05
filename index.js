112b0b86dee082b871bff651bff0c697935c782e
Vincent Weevers <mail@vincentweevers.nl>
2014-05-05 17:26:50 +0200

    includes, stream fix
    - sets basedir and filename options
      (for includes to work and debug purposes)
    - readme: warning about paths
    - pass file downstream when done compiling, not before
    - removed comment about streaming (code speaks for itself)
    - readme: simplified example

--- index.js
+++ index.js
@@ -6,16 +6,28 @@ module.exports = function(options) {
   if(!options) options = {};
 
   function modifyContents(file, cb) {
-    if (file.isNull()) return cb(null, file); // pass along
-    if (file.isStream()) return cb(new Error("gulp-blade: Streaming not supported")); // pass error if streaming is not supported
+    if (file.isNull())
+      return cb(null, file) // pass along
+    if (file.isStream())
+      return cb(new Error("gulp-blade: Streaming not supported"))
 
-    blade.compile(file.contents.toString(), options, function(err, res){
+    var localOptions = {
+      filename: file.path,  // required for blade includes
+      basedir: file.base    // typically where a glob starts
+    }
+
+    for(var k in options)
+      localOptions[k] = options[k]
+
+    blade.compile(file.contents.toString(), localOptions, function(err, res){
       if(err) return cb(err);
+
       file.contents = new Buffer(res.toString());
       file.path = gutil.replaceExtension(file.path, '.js');
+
+      cb(null, file);
     });
-    cb(null, file);
   }
 
   return map(modifyContents);
-};
+}
